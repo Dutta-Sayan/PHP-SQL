c2f168ccd7be22ce13fdffbd0fbef31deb67a8ec
Dutta-Sayan <sayan.dutta@innoraft.com>
2024-04-01 14:56:54 +0530

    FT2-109-2: Changed the filename.

--- Data.php
+++ Data.php
@@ -1,28 +1,48 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+// Including the Connect.php and Validate.php files.
 require_once 'Connect.php';
 require_once 'Validate.php';
+
 require_once realpath(__DIR__ . "/vendor/autoload.php");
+// Using the Dotenv package to load environment variables from .env file.
 use Dotenv\Dotenv;$dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
+// Storing the servername.
 $servername = $_ENV['SERVERNAME'];
+// Storing the username.
 $username = $_ENV['USERNAME'];
+// Storing the password.
 $password = $_ENV['PASSWORD'];
+// Storing the database name.
 $dbname = $_ENV['DATABASE'];
 
+// Storing the object of Connect class.
 $conn = new Connect($servername, $username, $password, $dbname);
+// Storing the object of Validate class.
 $valid = new Validate();
 
-if($_SERVER['REQUEST_METHOD'] == 'POST') {
+if(isset($_POST['submit'])) {
+  // Stores the employee first name.
   $empFname = trim($_POST['fname']);
+  // Stores the employee last name.
   $empLname = trim($_POST['lname']);
+  // Stores the employee code.
   $empCode = trim($_POST['code']);
+  // Stores the employee code name.
   $empCodeName = trim($_POST['codeName']);
+  // Stores the employee Id.
   $empId = trim($_POST['id']);
+  // Stores the employee domain.
   $empDomain = trim($_POST['domain']);
+  // Stores the employee salary.
   $empSalary = trim($_POST['salary']);
+  // Stores the employee percent.
   $empPercentile = trim($_POST['percent']);
 
+  // Function calls for different validation checks for the input fields values.
   $fnameErr = $valid->isValidFname($empFname);
   $lnameErr = $valid->isValidLname($empLname);
   $codeErr = $valid->isValidCode($empCode);
@@ -32,12 +52,12 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
   $salaryErr = $valid->isValidSalary($empSalary);
   $percentileErr = $valid->isValidPercentile($empPercentile);
 
+  // If all error messages are empty, then insert data into database.
   if($fnameErr=="" && $lnameErr=="" && $codeErr=="" && $codeNameErr=="" && 
   $idErr=="" && $domainErr=="" && $salaryErr=="" && $percentileErr=="") {
     $conn->insertCodeTable($empCode, $empCodeName, $empDomain);
     $conn->insertSalaryTable($empId, $empSalary, $empCode);
     $conn->insertDetailsTable($empId, $empFname, $empLname, $empPercentile);
-    $query1 = $conn->query1();
   }
 
 }
